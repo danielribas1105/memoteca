1a563cf7a84c2dfddf3ea6a76cbb5f21b3de52c1
Daniel <danielribas1105@gmail.com>
2023-05-18 13:17:25 -0300

    update components

--- src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CriarPensamentoComponent implements OnInit {
 
+  pensamento = {
+    id: '1',
+    conteudo: 'Aprendendo Angular',
+    autoria: 'Dev',
+    modelo: ''
+  }
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  criarPensamento() {
+    alert("Card salvo com sucesso!!!")
+  }
+
+  cancelPensamento() {
+    alert("Ação cancelada!!!")
   }
 
 }
