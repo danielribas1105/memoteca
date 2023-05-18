29efc90e9ca4efa578682b3f10155d19d38b6fed
Daniel <danielribas1105@gmail.com>
2023-05-18 11:08:14 -0300

    create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CabecalhoComponent } from './components/cabecalho/cabecalho.component';
 import { RodapeComponent } from './components/rodape/rodape.component';
-import { TituloComponent } from './components/titulo/titulo.component';
+import { CriarPensamentoComponent } from './components/pensamentos/criar-pensamento/criar-pensamento.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CabecalhoComponent,
     RodapeComponent,
-    TituloComponent
+    CriarPensamentoComponent
   ],
   imports: [
     BrowserModule
