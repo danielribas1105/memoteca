df5892e29f0df06aabff375fc62244deb5c7eba6
Daniel <danielribas1105@gmail.com>
2023-05-24 22:55:30 -0300

    update components

--- src/app/components/pensamentos/editar-pensamento/editar-pensamento.component.ts
+++ src/app/components/pensamentos/editar-pensamento/editar-pensamento.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Pensamento } from '../pensamento';
 import { PensamentoService } from '../pensamento.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
@@ -13,13 +12,6 @@ export class EditarPensamentoComponent implements OnInit {
 
   formulario!: FormGroup;
 
-  pensamento: Pensamento = {
-    id: 0,
-    conteudo: '',
-    autoria: '',
-    modelo: ''
-  }
-
   constructor(
     private service: PensamentoService,
     private router: Router,
@@ -30,27 +22,25 @@ export class EditarPensamentoComponent implements OnInit {
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id')
     this.service.buscarPorId(parseInt(id!)).subscribe((pensamento) => {
-      this.pensamento = pensamento
-    })
-    this.formulario = this.formBuilder.group({
-      conteudo: ['', Validators.compose([
-        Validators.required,
-        Validators.pattern(/(.|\s)*\S(.|\s)*/)
-      ])],
-      autoria: ['', Validators.compose([
-        Validators.required,
-        Validators.minLength(3)
-      ])],
-      modelo: ['modelo']
+      this.formulario = this.formBuilder.group({
+        id: [pensamento.id],
+        conteudo: [pensamento.conteudo, Validators.compose([
+          Validators.required,
+          Validators.pattern(/(.|\s)*\S(.|\s)*/)
+        ])],
+        autoria: [pensamento.autoria, Validators.compose([
+          Validators.required,
+          Validators.minLength(3)
+        ])],
+        modelo: [pensamento.modelo]
+      })
     })
   }
 
   editarPensamento() {
-    if(this.formulario.valid){
-      this.service.editar(this.formulario.value).subscribe(() => {
-        this.router.navigate(['/listarPensamento'])
-      })
-    }
+    this.service.editar(this.formulario.value).subscribe(() => {
+      this.router.navigate(['/listarPensamento'])
+    })
   }
 
   cancelar() {
