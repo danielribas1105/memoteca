d46995d5f8d2f900d42f26e6610cf0fe1372e6cc
Daniel <danielribas1105@gmail.com>
2023-05-24 23:45:54 -0300

    update components

--- src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -9,32 +9,24 @@ import { PensamentoService } from '../pensamento.service';
 })
 export class ListarPensamentoComponent implements OnInit {
 
-  listaPensamentos: Pensamento[] = [
-    {
-      id: 1,
-      conteudo: 'Passo informações para componente filho',
-      autoria: 'Componente Pai',
-      modelo: 'modelo1'
-    },
-    {
-      id: 2,
-      conteudo: 'Minha propriedade é decorada com @Input',
-      autoria: 'Componente Filho',
-      modelo: 'modelo2'
-    },
-    {
-      id: 3,
-      conteudo: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
-      autoria: 'Texto grande',
-      modelo: 'modelo3'
-    }
-  ];
+  listaPensamentos: Pensamento[] = [];
+  paginaAtual: number = 1;
+  haMaisPensamentos: boolean = true;
 
   constructor(private service: PensamentoService) { }
 
   ngOnInit(): void {
-    this.service.listar().subscribe((listaPensamentos) => {
+    this.service.listar(this.paginaAtual).subscribe((listaPensamentos) => {
       this.listaPensamentos = listaPensamentos
+    })
+  }
+
+  carregarMaisPensamentos() {
+    this.service.listar(++this.paginaAtual).subscribe(listaPensamentos => {
+      this.listaPensamentos.push(...listaPensamentos);
+      if(!listaPensamentos.length){
+        this.haMaisPensamentos = false;
+      }
     })
   }
 
